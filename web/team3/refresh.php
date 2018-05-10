575a3b6fd7cd4fc93bc18c125fb86d3b83f236ee
ambersto <sto12021@byui.edu>
2018-05-10 16:03:00 -0600

    Testing sessions

---
+++ web/team3/refresh.php
@@ -0,0 +1,11 @@
+<? php
+
+session_start()
+
+if(isset($_SESSION["timesVisited"))
+	$_SESSION["timesVisited"] += 1;
+else
+	$_SESSION["timesVisited"] = 0;
+
+echo('You have visited this page '.$_SESSION["timesVisited"].' times.')
+?>
