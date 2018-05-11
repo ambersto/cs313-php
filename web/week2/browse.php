d4a4b3904c6e1ba36ae60799e1f4831c8e26a65e
ambersto <sto12021@byui.edu>
2018-05-11 17:50:45 -0600

    Testing store page

---
+++ web/week2/browse.php
@@ -0,0 +1,15 @@
+<head>
+	<title>Phil's</title>
+</head>
+<body>
+<h1>Phil's Corner Market</h1>
+<h2>Select items to purchase below:</h2>
+<p>
+	<form action="add.php">
+		<input type="hidden" name="item" value="apple">
+		<input type="submit">
+	</form>
+</p>
+
+	
+</body>
