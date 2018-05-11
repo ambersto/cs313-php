43824281d1d15bb71122809cbeeca474234737c7
ambersto <sto12021@byui.edu>
2018-05-11 17:58:02 -0600

    Testing store page

--- web/week2/add.php
+++ web/week2/add.php
@@ -1,4 +1,4 @@
-<script type='text/javascript'>alert('Item added!');</script>
 <?php 
+echo "<script type='text/javascript'>alert('Item added!');</script>";
 header("Location: {$_SERVER['HTTP_REFERER']}");
 ?>
