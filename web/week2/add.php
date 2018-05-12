778f4ae471abfe2774d6a5a7c1f15ea6dc446094
ambersto <sto12021@byui.edu>
2018-05-11 19:56:08 -0600

    Testing store page

--- web/week2/add.php
+++ web/week2/add.php
@@ -6,8 +6,9 @@
 <body>
 
 <?php
-	echo "<h1>Your ordered:</h1>";
-	foreach ($_POST["item"] as $selected) {
+	echo "<h1>Hello " . $_POST["username"] . "</h1>";
+	echo "<h1>Your choice is: " . $_POST["item"] . "</h1>";
+	foreach ($_POST["berry"] as $selected) {
 		echo "<h1>* " . $selected . "</h1>";
 	}
 //header("Location: {$_SERVER['HTTP_REFERER']}");
--- web/week2/browse.php
+++ web/week2/browse.php
@@ -3,15 +3,29 @@
 </head>
 <body>
 <h1>Phil's Corner Market</h1>
-<h2>Select items to purchase below</h2>
+<h2>Select items to purchase below:</h2>
+
+<form method="post" action="add.php">
+	<input type="text" name="username">
+	<input type="submit">
+</form>
+
+Apples<br>
+<form method="post" action="add.php">
+	<input type="submit" name="item" value="apple">
+</form>
+
+Bananas<br>
+<form method="post" action="add.php">
+	<input type="hidden" name="item" value="banana">
+	<input type="Submit">
+</form>
 
 <form method="post" action="add.php">
 
 <p>Which of the following would you like to order:</p>
-<input type="checkbox" name="item[]" value="Apples" id = "app"><label for="app">Apples</label><br>
-<input type="checkbox" name="item[]" value="Bananas" id = "ban"><label for="ban">Bananas</label><br>
-<input type="checkbox" name="item[]" value="Strawberries" id = "str"><label for="str">Strawberries</label><br>
-<input type="checkbox" name="item[]" value="Blueberries" id = "blu"><label for="blu">Blueberries</label><br>
+<input type="checkbox" name="berry[]" value="Strawberries" id = "str"><label for="str">strawberries</label>
+<input type="checkbox" name="berry[]" value="Blueberries" id = "blu"><label for="blu">blueberries</label>
 <input type="Submit">
 
 </form>
