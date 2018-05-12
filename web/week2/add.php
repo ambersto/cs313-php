7029f4d259dfcf7fb872cfb8d3d7e47850c1b31f
ambersto <sto12021@byui.edu>
2018-05-11 18:42:49 -0600

    Testing store page

--- web/week2/add.php
+++ web/week2/add.php
@@ -8,7 +8,7 @@
 <?php
 	echo "<h1>Hello " . $_POST["username"] . "</h1>";
 	echo "<h1>Your choice is: " . $_POST["item"] . "</h1>";
-	foreach ($_POST["berry"]) {
+	if (isset($_POST("berry"))) {
 		echo "<h1>Your berry is: " . $_POST["berry"] . "</h1>";
 	}
 //header("Location: {$_SERVER['HTTP_REFERER']}");
