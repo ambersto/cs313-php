e2522a7fb17d0bb277c62a0ade081b26af420811
ambersto <sto12021@byui.edu>
2018-05-11 20:13:01 -0600

    Testing store page

--- web/week2/add.php
+++ web/week2/add.php
@@ -1,15 +1,16 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Add Item</title>
-</head>
-<body>
-
 <?php
 	echo "<h1>Your choice is: " . $_POST["item"] . "</h1>";
 //header("Location: {$_SERVER['HTTP_REFERER']}");
+
+//session_start();
+
+$fruit = $_POST["item"];
+echo "<h1>It really is: " . $fruit . "</h1>";
+
+/*if(isset($_SESSION["timesVisited"]))
+	$_SESSION["timesVisited"] += 1;
+else
+	$_SESSION["timesVisited"] = 0;
+
+echo('<span style="color: blue;">You have visited this page '.$_SESSION["timesVisited"].' times.</span>');*/
 ?>
-
-</body>
-</html>
-
