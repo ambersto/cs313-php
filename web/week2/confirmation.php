3547f82cf6825370abeebd439691dc24d5dc538d
ambersto <sto12021@byui.edu>
2018-05-11 21:24:36 -0600

    Adding cart page

--- web/week2/confirmation.php
+++ web/week2/confirmation.php
@@ -5,14 +5,21 @@
 </head>
 <body>
 <h1>Confirmation</h1>
-<h2>Your purchase has been made!</h2>
+<h2>Your purchase has been completed!</h2>
 
-<p>Shipping address:<br>	
 <?php
+echo "<p>Items ordered:<br><br>";
+foreach ($_SESSION as $fruit => $quantity) {
+	echo "You ordered "  . $quantity . " " . $fruit . "(s).<br>";
+}
+
+echo "<p>Shipping address:<br><br>	";
 echo $_POST["username"] . "<br>" . $_POST["street"] . "<br>";
 echo $_POST["city"] . ", " . $_POST["state"] . " " . $_POST["zip"];
 ?>
 </p>
+
+<h2>Thank you for shopping with Phil's Corner Market!</h2>
 
 </body>
 </html>
