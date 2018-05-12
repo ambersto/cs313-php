cb33a24a4f6a13373d5a75a186b56dda37095e92
ambersto <sto12021@byui.edu>
2018-05-11 20:31:04 -0600

    Testing store page

--- web/week2/cart.php
+++ web/week2/cart.php
@@ -1,5 +1,5 @@
 <?php
-echo "This page works I think.";
+echo "This is how many apples I have: " . $_SESSION["apple"];
 
 foreach ($_SESSION as $fruit => $quantity) {
 	echo "You ordered "  . $quantity . " " . $fruit . "(s).";
