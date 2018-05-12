80210e2343bf6939c8ed61102092bf28e347b813
ambersto <sto12021@byui.edu>
2018-05-11 21:18:13 -0600

    Adding cart page

--- web/week2/checkout.php
+++ web/week2/checkout.php
@@ -7,19 +7,20 @@
 <h1>Checkout</h1>
 <h2>Please enter your shipping address.</h2>
 <form method="post" action="confirmation.php">
-<h3>Name: </h3>
-<input type="text" name="username">
-<h3>Street Address: </h3>
-<input type="text" name="street">
-<h3>City: </h3>
-<input type="text" name="city">
-<h3>State: </h3>
-<input type="text" name="state">
-<h3>Zip Code: </h3>
-<input type="text" name="zip">
+<h3>Name: 
+<input type="text" name="username"></h3>
+<h3>Street Address: 
+<input type="text" name="street"></h3>
+<h3>City: 
+<input type="text" name="city"></h3>
+<h3>State: 
+<input type="text" name="state"></h3>
+<h3>Zip Code: 
+<input type="text" name="zip"></h3>
 <br><br>
-<button type="submit">Continue with Checkout</button><br>
+<button type="submit">Continue with Checkout</button>
 </form>
+<br>
 <form action="cart.php"><button type="submit">Back to Cart</button></form>
 </body>
 </html>
--- web/week2/confirmation.php
+++ web/week2/confirmation.php
@@ -6,5 +6,13 @@
 <body>
 <h1>Confirmation</h1>
 <h2>Your purchase has been made!</h2>
+
+<p>Shipping address:<br>	
+<?php
+echo $_POST["username"] . "<br>" . $_POST["street"] . "<br>";
+echo $_POST["city"] . ", " . $_POST["state"] . " " . $_POST["zip"];
+?>
+</p>
+
 </body>
 </html>
