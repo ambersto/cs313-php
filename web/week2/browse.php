1897bae5fc10c933b212d60a12bd49b010bb938d
ambersto <sto12021@byui.edu>
2018-05-11 18:53:34 -0600

    Testing store page

--- web/week2/browse.php
+++ web/week2/browse.php
@@ -3,28 +3,17 @@
 </head>
 <body>
 <h1>Phil's Corner Market</h1>
-<h2>Select items to purchase below:</h2>
+<h2>Select items to purchase below</h2>
 
 <form method="post" action="add.php">
-	<input type="text" name="username">
-	<input type="submit">
-</form>
 
-Apples<br>
-<form method="post" action="add.php">
-	<input type="submit" name="item" value="apple">
-</form>
-
-Bananas<br>
-<form method="post" action="add.php">
-	<input type="hidden" name="item" value="banana">
-	<input type="Submit">
-</form>
-
-<form method="post" action="add.php">
-<p>Take your pick of berries:</p>
-<input type="checkbox" name="berry[]" value="Strawberries" id = "str"><label for="str">strawberries</label>
-<input type="checkbox" name="berry[]" value="Blueberries" id = "blu"><label for="blu">blueberries</label>
+<p>Which of the following would you like to order:</p>
+<input type="checkbox" name="item[]" value="Apples" id = "app"><label for="app">apples</label>
+<input type="checkbox" name="item[]" value="Bananas" id = "ban"><label for="ban">Bananas</label>
+<input type="checkbox" name="item[]" value="Strawberries" id = "str"><label for="str">Strawberries</label>
+<input type="checkbox" name="item[]" value="Blueberries" id = "blu"><label for="blu">Blueberries</label>
 <input type="Submit">
+
 </form>
+
 </body>
