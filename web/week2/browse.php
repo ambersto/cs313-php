c18795e51e6db473e8a591abbcf30cfbcfb0da74
ambersto <sto12021@byui.edu>
2018-05-11 18:54:53 -0600

    Testing store page

--- web/week2/browse.php
+++ web/week2/browse.php
@@ -8,10 +8,10 @@
 <form method="post" action="add.php">
 
 <p>Which of the following would you like to order:</p>
-<input type="checkbox" name="item[]" value="Apples" id = "app"><label for="app">apples</label>
-<input type="checkbox" name="item[]" value="Bananas" id = "ban"><label for="ban">Bananas</label>
-<input type="checkbox" name="item[]" value="Strawberries" id = "str"><label for="str">Strawberries</label>
-<input type="checkbox" name="item[]" value="Blueberries" id = "blu"><label for="blu">Blueberries</label>
+<input type="checkbox" name="item[]" value="Apples" id = "app"><label for="app">Apples</label><br>
+<input type="checkbox" name="item[]" value="Bananas" id = "ban"><label for="ban">Bananas</label><br>
+<input type="checkbox" name="item[]" value="Strawberries" id = "str"><label for="str">Strawberries</label><br>
+<input type="checkbox" name="item[]" value="Blueberries" id = "blu"><label for="blu">Blueberries</label><br>
 <input type="Submit">
 
 </form>
