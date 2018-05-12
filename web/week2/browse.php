8304984383fde3001520b46bfe33678c8e2e7084
ambersto <sto12021@byui.edu>
2018-05-11 18:12:25 -0600

    Testing store page

--- web/week2/browse.php
+++ web/week2/browse.php
@@ -6,14 +6,14 @@
 <h2>Select items to purchase below:</h2>
 <p>
 Apples<br>
-	<form action="add.php">
+	<form method="post" action="add.php">
 		<input type="hidden" name="item" value="apple">
 		<input type="submit" value="Add">
 	</form>
 </p>
 <p>
 Bananas<br>
-	<form action="add.php">
+	<form method="post" action="add.php">
 		<input type="hidden" name="item" value="banana">
 		<input type="submit" value="Add">
 	</form>
