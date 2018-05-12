270f2585c33565b21e03cb8d3b305c58c169cba9
ambersto <sto12021@byui.edu>
2018-05-11 21:10:22 -0600

    Adding cart page

--- web/week2/checkout.php
+++ web/week2/checkout.php
@@ -5,7 +5,21 @@
 </head>
 <body>
 <h1>Checkout</h1>
-<form action="confirmation.php"><button type="submit">Continue with Checkout</button></form>
+<h2>Please enter your shipping address.</h2>
+<form method="post" action="confirmation.php">
+<h3>Name: </h3>
+<input type="text" name="username">
+<h3>Street Address: </h3>
+<input type="text" name="street">
+<h3>City: </h3>
+<input type="text" name="city">
+<h3>State: </h3>
+<input type="text" name="state">
+<h3>Zip Code: </h3>
+<input type="text" name="zip">
+<br><br>
+<button type="submit">Continue with Checkout</button><br>
+</form>
 <form action="cart.php"><button type="submit">Back to Cart</button></form>
 </body>
 </html>
