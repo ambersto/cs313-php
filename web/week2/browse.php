d3fca8f2ac4caefd4f811881fbecb8625975b761
ambersto <sto12021@byui.edu>
2018-05-11 21:34:20 -0600

    Adding cart page

--- web/week2/browse.php
+++ web/week2/browse.php
@@ -1,29 +1,33 @@
 <head>
-	<title>Phil's</title>
+	<title>Phil's Corner Market</title>
 </head>
 <body>
 <h1>Phil's Corner Market</h1>
 <h2>Select items to purchase below:</h2>
 
 <h3>Apple</h3>
+<img src="apple.jpg">
 <form method="post" action="add.php">
 	<input type="hidden" name="item" value="Apple">
 	<button type="submit">Add to Cart</button>
 </form>
 
 <h3>Banana</h3>
+<img src="banana.png">
 <form method="post" action="add.php">
 	<input type="hidden" name="item" value="Banana">
 	<button type="submit">Add to Cart</button>
 </form>
 
 <h3>Mango</h3>
+<img src="mango.jpg">
 <form method="post" action="add.php">
 	<input type="hidden" name="item" value="Mango">
 	<button type="submit">Add to Cart</button>
 </form>
 
 <h3>Orange</h3>
+<img src="orange.jpg">
 <form method="post" action="add.php">
 	<input type="hidden" name="item" value="Orange">
 	<button type="submit">Add to Cart</button>
