2ff092024b8a22a2d337c51b572de87253e06127
ambersto <sto12021@byui.edu>
2018-05-11 20:43:58 -0600

    Adding cart page

--- web/week2/add.php
+++ web/week2/add.php
@@ -3,15 +3,15 @@
 session_start();
 
 $fruit = $_POST["item"];
-echo "<h1>Your choice is: " . $fruit . "</h1>";
+//echo "<h1>Your choice is: " . $fruit . "</h1>";
 
 if(isset($_SESSION[$fruit]))
 	$_SESSION[$fruit] += 1;
 else
 	$_SESSION[$fruit] = 1;
 
-echo "Number of " . $fruit . "s: " . $_SESSION[$fruit];
+//echo "Number of " . $fruit . "s: " . $_SESSION[$fruit];
 
-//header("Location: {$_SERVER['HTTP_REFERER']}");
+header("Location: {$_SERVER['HTTP_REFERER']}");
 
 ?>
--- web/week2/cart.php
+++ web/week2/cart.php
@@ -2,9 +2,7 @@
 
 session_start();
 
-echo "This is how many apples I have: " . $_SESSION["Apple"];
-
 foreach ($_SESSION as $fruit => $quantity) {
-	echo "You ordered "  . $quantity . " " . $fruit . "(s).";
+	echo "You ordered "  . $quantity . " " . $fruit . "(s).<br>";
 }
 ?>
