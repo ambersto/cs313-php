8b7dcbe6ec9b43eea55797869f6d01df51e546c1
ambersto <sto12021@byui.edu>
2018-05-11 20:50:43 -0600

    Adding cart page

--- web/week2/cart.php
+++ web/week2/cart.php
@@ -1,8 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
 <?php
 
 session_start();
 
 foreach ($_SESSION as $fruit => $quantity) {
-	echo "You ordered "  . $quantity . " " . $fruit . "(s).<br>";
+	echo "<h2>You ordered "  . $quantity . " " . $fruit . "(s).</h2><br>";
 }
+
 ?>
+
+<br><br>
+<form action="browse.php"><button type="submit">Continue Shopping</button></form>
+<form action="checkout.php"><button type="submit">Checkout</button></form>
+
+</body>
+</html>
