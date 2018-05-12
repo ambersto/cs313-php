ef9f982e693b8b09226baccbeb73ddd11f44d6ff
ambersto <sto12021@byui.edu>
2018-05-11 20:54:51 -0600

    Adding cart page

--- web/week2/browse.php
+++ web/week2/browse.php
@@ -5,25 +5,25 @@
 <h1>Phil's Corner Market</h1>
 <h2>Select items to purchase below:</h2>
 
-Apples<br>
+<h2>Apples</h2>
 <form method="post" action="add.php">
 	<input type="hidden" name="item" value="Apple">
 	<button type="submit">Add to Cart</button>
 </form>
 
-Bananas<br>
+<h2>Bananas</h2>
 <form method="post" action="add.php">
 	<input type="hidden" name="item" value="Banana">
 	<button type="submit">Add to Cart</button>
 </form>
 
-Mango<br>
+<h2>Mango</h2>
 <form method="post" action="add.php">
 	<input type="hidden" name="item" value="Mango">
 	<button type="submit">Add to Cart</button>
 </form>
 
-Orange<br>
+<h2>Orange</h2>
 <form method="post" action="add.php">
 	<input type="hidden" name="item" value="Orange">
 	<button type="submit">Add to Cart</button>
--- web/week2/cart.php
+++ web/week2/cart.php
@@ -9,13 +9,14 @@
 session_start();
 
 foreach ($_SESSION as $fruit => $quantity) {
-	echo "<h2>You ordered "  . $quantity . " " . $fruit . "(s).</h2><br>";
+	echo "<h2>You ordered "  . $quantity . " " . $fruit . "(s).</h2>";
 }
 
 ?>
 
 <br><br>
 <form action="browse.php"><button type="submit">Continue Shopping</button></form>
+<br>
 <form action="checkout.php"><button type="submit">Checkout</button></form>
 
 </body>
