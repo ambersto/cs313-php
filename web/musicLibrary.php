7860894610cb010f9edd97a77e3886773a78aed5
ambersto <sto12021@byui.edu>
2018-05-24 15:54:11 -0600

    Adding music library php

---
+++ web/musicLibrary.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>FTH Music Library</title>
+</head>
+<body>
+<h1>FTH Music Library</h1>
+<h2>This is the an electronic compilation of the music for From the Heart Choir. Below is a list of all songs.</h2>
+
+<?php
+$dbUrl = getenv('DATABASE_URL');
+
+$dbopts = parse_url($dbUrl);
+
+$dbHost = $dbopts["host"];
+$dbPort = $dbopts["port"];
+$dbUser = $dbopts["user"];
+$dbPassword = $dbopts["pass"];
+$dbName = ltrim($dbopts["path"],'/');
+
+$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+foreach ($db->query('SELECT title,	composerID,	typeID,	isSoprano, isAlto, isTenor, isBass FROM song') as $row) {
+	
+	$composer = "";
+	$type = "";
+	
+	foreach ($db->query('SELECT id, firstName, lastName FROM composer') as $composerRow) {
+		if($composerRow['id'] == $row['composerID']) {
+			$composer += $composerRow['lastName'];
+		}
+	}
+	
+	foreach($db->query('SELECT id, name FROM type') as $typeRow){
+		if($typeRow['id'] == $row['typeID']) {
+			$type = $typeRow['name'];
+		}
+	}
+
+	echo '<span style="font-weight: bold;">';
+	echo $row['title'] . '</span> by ' . $row['composerID'];
+	echo '<br/>Type: ' . $type . '<br/>';
+}
+
+?>
+
+</body>
+</html>
