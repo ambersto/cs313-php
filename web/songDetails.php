bccae781178c9b076d4778e4f8c0d90ae2c9695c
ambersto <sto12021@byui.edu>
2018-05-24 23:08:55 -0600

    working on song details

--- web/songDetails.php
+++ web/songDetails.php
@@ -21,16 +21,25 @@ $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPass
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 $id = $_GET['id'];
-$query = ("SELECT * FROM song WHERE id=$id");
+$query = ("SELECT s.title, c.firstName, c.lastName, t.name, s.isSoprano, s.isAlto, s.isTenor, s.isBass FROM song s INNER JOIN composer c ON s.composerID=c.id INNER JOIN type t ON s.typeID=t.id WHERE s.id=$id");
 
 foreach ($db->query($query) as $row) {
-	echo 'Title is: ' . $row['title'] . '<br/>';
-}
-
-$querytwo = ("SELECT s.title, c.firstName, c.lastName, t.name, s.isSoprano, s.isAlto, s.isTenor, s.isBass FROM song s INNER JOIN composer c ON s.composerID=c.id INNER JOIN type t ON s.typeID=t.id WHERE s.id=$id");
-
-foreach ($db->query($querytwo) as $row) {
-	echo 'Title Two is: ' . $row['title'] . '<br/>';
+	echo '<h2>' . $row['title'] . '</h2>
+	<h3>By ' . $row['firstname'] . ' ' . $row['lastname'] . '</h3>
+	<ul><li>' . $row['name'] . '</li><li>';
+	if($row['issoprano']) {
+		echo 'S';
+	}
+	if($row['isalto']) {
+		echo 'A';
+	}
+	if($row['istenor']) {
+		echo 'T';
+	}
+	if($row['isbass']) {
+		echo 'B';
+	}
+	echo '</li></ul>';
 }
 
 ?>
