2179edc21f3be2d20dcbf8a8b2d2aba93d05e8d4
ambersto <sto12021@byui.edu>
2018-05-24 22:32:10 -0600

    Adding music library php

--- web/songDetails.php
+++ web/songDetails.php
@@ -20,7 +20,9 @@ $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPass
 
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$query = $db->query("SELECT * FROM song WHERE id = 3");
+echo '<h2>ID is: ' . $_GET['id'];
+
+$query = $db->query("SELECT * FROM song WHERE id = ".$_GET['id']);
 $composer = $db->query('SELECT firstName, lastName FROM composer WHERE id = '.$row['composerid']);
 $type = $db->query('SELECT name FROM type WHERE id = '.$row['typeid']);
 
