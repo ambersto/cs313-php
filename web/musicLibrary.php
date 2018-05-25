9239b8eb030002cf79c1655cd78816a3b8cae583
ambersto <sto12021@byui.edu>
2018-05-24 23:12:45 -0600

    working on song details

--- web/musicLibrary.php
+++ web/musicLibrary.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<title>FTH Music Library</title>
+	<link rel="stylesheet" type="text/css" href="style.css"/>
 </head>
 <body>
 <h1>FTH Music Library</h1>
--- web/songDetails.php
+++ web/songDetails.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<title>Song Details</title>
+	<link rel="stylesheet" type="text/css" href="style.css"/>
 </head>
 <body>
 <h1>Song Details</h1>
