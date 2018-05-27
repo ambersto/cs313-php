73ec65c6e66743de4dacca833e6d094b2e9f33da
ambersto <sto12021@byui.edu>
2018-05-26 19:17:13 -0600

    adding buttons and view song page

--- web/musicLibrary.php
+++ web/musicLibrary.php
@@ -6,36 +6,14 @@
 </head>
 <body>
 <h1>FTH Music Library</h1>
-<h2>This is the an electronic compilation of the music for From the Heart choir. Below is a list of all songs.</h2>
+<a href="musicLibrary.php"><div id="button">Home</div></a>
+<a href="viewSongs.php"><div id="button">View Songs</div></a>
+<div id="button">View Performances</div>
+<div id="button">Search Music</div>
+<div id="button">Add Songs</div>
+<h2>This is the an electronic compilation of the music for From the Heart choir. Select an option from the menu to the left.</h2>
 
-<?php
-$dbUrl = getenv('DATABASE_URL');
 
-$dbopts = parse_url($dbUrl);
-
-$dbHost = $dbopts["host"];
-$dbPort = $dbopts["port"];
-$dbUser = $dbopts["user"];
-$dbPassword = $dbopts["pass"];
-$dbName = ltrim($dbopts["path"],'/');
-
-$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-$query = "SELECT s.id, s.title, c.firstName, c.lastName FROM song s INNER JOIN composer c ON s.composerID = c.id";
-echo '<ul>';
-
-foreach ($db->query($query) as $row) {
-	echo '<a href="songDetails.php?id=' . $row['id'] . '">';
-	echo '<li><span style="font-weight: bold;">';
-	echo $row['title'] . '</span></a><span style="font-style: italic;"> by ' . $row['firstname'] . ' ';
-	echo $row['lastname'] . '</span></li>';
-}
-
-echo '</ul>';
-
-?>
 
 </body>
 </html>
