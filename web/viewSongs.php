d62bcda24464fe4114a3aa5e73547fb14fff6c73
ambersto <sto12021@byui.edu>
2018-05-26 22:07:01 -0600

    using prepare

--- web/viewSongs.php
+++ web/viewSongs.php
@@ -28,7 +28,22 @@ $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPass
 
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$query = "SELECT s.id, s.title, c.firstName, c.lastName FROM song s INNER JOIN composer c ON s.composerID = c.id";
+$stmt = $db->prepare('SELECT s.id, s.title, c.firstName, c.lastName FROM song s INNER JOIN composer c ON s.composerID = c.id WHERE id=:id AND title=:title AND firstName=:firstname AND lastName=:lastname');
+$stmt->execute(array(':name' => $name, ':id' => $id));
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+echo '<ul style="position: relative; margin-left:250px; list-style-type:none;">';
+
+foreach ($rows as $row) {
+	echo '<a href="songDetails.php?id=' . $row['id'] . '">';
+	echo '<li><span style="font-weight: bold;">';
+	echo $row['title'] . '</span></a><span style="font-style: italic;"> by ' . $row['firstname'] . ' ';
+	echo $row['lastname'] . '</span></li>';
+}
+
+echo '</ul>';
+
+/*$query = "SELECT s.id, s.title, c.firstName, c.lastName FROM song s INNER JOIN composer c ON s.composerID = c.id";
 echo '<ul style="position: relative; margin-left:250px; list-style-type:none;">';
 
 foreach ($db->query($query) as $row) {
@@ -38,7 +53,7 @@ foreach ($db->query($query) as $row) {
 	echo $row['lastname'] . '</span></li>';
 }
 
-echo '</ul>';
+echo '</ul>';*/
 
 ?>
 
