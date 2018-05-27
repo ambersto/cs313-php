ca71074ea1222347524388684d7ee6784d69db90
ambersto <sto12021@byui.edu>
2018-05-26 20:31:33 -0600

    Adding to performance page

--- web/viewPerformances.php
+++ web/viewPerformances.php
@@ -11,7 +11,7 @@
 <a href="viewPerformances.php"><div id="button">View Performances</div></a>
 <div id="button">Search Music</div>
 <div id="button" class="last">Add Songs</div>
-<h2>This is the an electronic compilation of the music for From the Heart choir. Below is a list of all songs.</h2>
+<h2>This is the an electronic compilation of the music for From the Heart choir. Below is a list of performances and the songs that were sung at each one.</h2>
 
 <?php
 $dbUrl = getenv('DATABASE_URL');
@@ -28,7 +28,22 @@ $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPass
 
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$query = "SELECT p.semester, p.year, s.id, s.title, c.firstName, c.lastName FROM performance p INNER JOIN performanceList pl ON p.id = pl.performanceID INNER JOIN song s ON pl.songID = s.id INNER JOIN composer c ON s.composerID = c.id";
+$query = "SELECT id, year, semester FROM performance";
+echo '<ul style="position: relative; margin-left:250px; list-style-type:none;">';
+
+foreach ($db->query($query) as $row) {
+	echo '<li>' . $row['semester'] . ' ' . $row['year'] . ' Performance:</li>';
+	$performanceID = $row['id'];
+
+	foreach ($db->query("SELECT s.id, s.title, c.firstName, c.lastName FROM performanceList pl WHERE pl.performanceID = $performanceID INNER JOIN song s ON pl.songID = s.id INNER JOIN composer c ON s.composerID = c.id") as $songRow) {
+		echo '<a href="songDetails.php?id=' . $songRow['id'] . '">';
+		echo '<li><span style="font-weight: bold;">';
+		echo $songRow['title'] . '</span></a><span style="font-style: italic;"> by ' . $songRow['firstname'] . ' ';
+		echo $songRow['lastname'] . '</span></li>';
+	}
+}
+
+/*$query = "SELECT p.semester, p.year, s.id, s.title, c.firstName, c.lastName FROM performance p INNER JOIN performanceList pl ON p.id = pl.performanceID INNER JOIN song s ON pl.songID = s.id INNER JOIN composer c ON s.composerID = c.id";
 echo '<ul style="position: relative; margin-left:250px; list-style-type:none;">';
 
 foreach ($db->query($query) as $row) {
@@ -37,7 +52,7 @@ foreach ($db->query($query) as $row) {
 	echo '<li><span style="font-weight: bold;">';
 	echo $row['title'] . '</span></a><span style="font-style: italic;"> by ' . $row['firstname'] . ' ';
 	echo $row['lastname'] . '</span></li>';
-}
+}*/
 
 echo '</ul>';
 
