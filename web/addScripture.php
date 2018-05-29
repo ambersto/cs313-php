a10756c324ab4ebdc0b7e8d7fd4c03c035ef7aca
ambersto <sto12021@byui.edu>
2018-05-29 16:29:10 -0600

    insert/add scripture pages

--- web/addScripture.php
+++ web/addScripture.php
@@ -21,11 +21,16 @@ $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPass
 
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$book = $_POST['book'];
+/*$book = $_POST['book'];
 $chapter = $_POST['chapter'];
 $verse = $_POST['verse'];
 $content = $_POST['content'];
-$topicID = $_POST['id'];
+$topicID = $_POST['id'];*/
+
+$book = 'Test';
+$chapter = 1;
+$verse = 11;
+$content = "Here is test content";
 
 $stmt = $pdo->prepare('INSERT INTO scriptures (book,chapter,verse,content) VALUES (:book,:chapter,:verse,:content)');
 $stmt->bindValue(':book', $book, PDO::PARAM_STR);
@@ -37,7 +42,7 @@ $stmt->execute();
 $scriptureID = $pdo->lastInsertId('scripture_id_seq');
 
 foreach ($_POST['topics'] as $topicID) {
-	$stmt = $pdo->prepare('INSERT INTO topics (scriptureID, topicID) VALUES (:topicID, :scriptureID)');
+	$stmt = $pdo->prepare('INSERT INTO scriptureTopics (scriptureID, topicID) VALUES (:topicID, :scriptureID)');
 	$stmt->bindValue(':topicID', $topicID, PDO::PARAM_INT);
 	$stmt->bindValue(':scriptureID', $scriptureID, PDO::PARAM_INT);
 	$stmt->execute();	
