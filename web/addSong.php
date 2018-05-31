17fe5979dd9db64622d4859f04ae9f3e3876f5ad
ambersto <sto12021@byui.edu>
2018-05-31 16:16:28 -0600

    working on adding song

--- web/addSong.php
+++ web/addSong.php
@@ -16,7 +16,22 @@
 <br><br>
 
 <?php
+// Connect with database
+$dbUrl = getenv('DATABASE_URL');
 
+$dbopts = parse_url($dbUrl);
+
+$dbHost = $dbopts["host"];
+$dbPort = $dbopts["port"];
+$dbUser = $dbopts["user"];
+$dbPassword = $dbopts["pass"];
+$dbName = ltrim($dbopts["path"],'/');
+
+$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// Initialize variables
 $songTitle = test_input($_POST["songTitle"]);
 $composerFirstName = test_input($_POST["composerFirstName"]);
 $composerLastName = test_input($_POST["composerLastName"]);
@@ -25,7 +40,9 @@ $isSoprano;
 $isAlto;
 $isTenor;
 $isBass;
+$composerID;
 
+// Display song information
 echo "<h3>$songTitle<br>
 	By $composerFirstName $composerLastName<br>
 	Type: $songType<br>
@@ -35,27 +52,68 @@ if (isset($_POST['isSoprano'])) {
 	$isSoprano = true;
 	echo "S";
 }
+else {
+	$isSoprano = false;
+}
 if (isset($_POST['isAlto'])) {
 	$isAlto = true;
 	echo "A";
+}
+else {
+	$isAlto = false;
 }
 if (isset($_POST['isTenor'])) {
 	$isTenor = true;
 	echo "T";
 }
+else {
+	$isTenor = false;
+}
 if (isset($_POST['isBass'])) {
 	$isBass = true;
 	echo "B";
 }
+else {
+	$isBass = false;
+}
 
 echo "</h3>";
 
+// Filter input
 function test_input($data) {
   $data = trim($data);
   $data = stripslashes($data);
   $data = htmlspecialchars($data);
   return $data;
 }
+
+// Insert info into database
+$composerQuery = "SELECT * FROM composer";
+
+foreach ($db->query($query) as $row) {
+	if($row['firstName']==$composerFirstName && $row['lastName']==$composerLastName) {
+		$composerID = $row['id'];
+	}
+}
+
+if(!isset($composerID)) {
+	$stmt = $db->prepare('INSERT INTO composer (firstName,lastName) VALUES (:firstName, :lastName)');
+	$stmt->bindValue(':firstName', $composerFirstName, PDO::PARAM_STR);
+	$stmt->bindValue(':lastName', $composerLastName, PDO::PARAM_STR);
+	$stmt->execute();
+	$composerID = $db->lastInsertId('composer_id_seq');
+}
+
+$stmt = $db->prepare('INSERT INTO song (title, composerID, typeID, isSoprano, isAlto, isTenor, isBass) VALUES (:title, :composerID, (SELECT id FROM type WHERE name=:type), :isSoprano, :isAlto, :isTenor, :isBass)');
+$stmt->bindValue(':title', $songTitle, PDO::PARAM_STR);
+$stmt->bindValue(':composerID', $composerID, PDO::PARAM_INT);
+$stmt->bindValue(':type', $songType, PDO::PARAM_STR);
+$stmt->bindValue(':isSoprano', $isSoprano, PDO::PARAM_BOOL);
+$stmt->bindValue(':isAlto', $isAlto, PDO::PARAM_BOOL);
+$stmt->bindValue(':isTenor', $isTenor, PDO::PARAM_BOOL);
+$stmt->bindValue(':isBass', $isBass, PDO::PARAM_BOOL);
+$stmt->execute();
+
 ?>
 
 </body>
--- web/enterSong.php
+++ web/enterSong.php
@@ -19,21 +19,21 @@
 <form method="post" action="addSong.php">
 
 <h3>Title of song: 
-<input type="text" name="songTitle"></h3>
+<input type="text" name="songTitle"></h3><br>
 
 <h3>Composer's first name: 
-<input type="text" name="composerFirstName"></h3>
+<input type="text" name="composerFirstName"></h3><br>
 
 <h3>Composer's last name: 
-<input type="text" name="composerLastName"></h3>
+<input type="text" name="composerLastName"></h3><br>
 
-<h3>Type of song: 
-<input type="radio" name="songType" value="1"> Choral<br>
-<input type="radio" name="songType" value="2"> Solo<br>
-<input type="radio" name="songType" value="3"> Duet<br>
-<input type="radio" name="songType" value="4"> Group</h3>
+<h3>Type of song:<br>
+<input type="radio" name="songType" value="Choral"> Choral<br>
+<input type="radio" name="songType" value="Solo"> Solo<br>
+<input type="radio" name="songType" value="Duet"> Duet<br>
+<input type="radio" name="songType" value="Group"> Group<br></h3>
 
-<h3>Voice parts (i.e. SATB, SA, SAT, TB, etc.):
+<h3>Voice parts:<br>
 <input type="checkbox" name="isSoprano"> Soprano<br>
 <input type="checkbox" name="isAlto"> Alto<br>
 <input type="checkbox" name="isTenor"> Tenor<br>
